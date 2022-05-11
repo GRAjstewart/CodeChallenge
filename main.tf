6a31a67c4ba2aafa0f063712c8c1dcbc4fed6d79
John Stewart <jstewart@taos.com>
2022-05-11 17:11:10 -0400

    first we need network

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "random_pet" "rg-name" {
+  prefix    = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name      = random_pet.rg-name.id
+  location  = var.resource_group_location
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "public_key_openssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+module "network" {
+ source = "./modules/azure_network"
+ rg_name = azurerm_resource_group.rg.name 
+ rg_location = var.resource_group_location
+}
